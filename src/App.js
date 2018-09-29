523742a6a272252339ed4f5c7e9a4063afb7f404
Code Apprentice <codeguru42@gmail.com>
2018-09-29 16:54:10 -0600

    Request stock data from alphavantage.com API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
+import * as d3 from "d3";
 import './App.css';
 import {Controls} from "./Controls";
 import {Chart} from "./Chart";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    }
+  }
+
+
   onSubmit = values => {
-    alert(`symbol:${values.symbol}`);
+    const {symbol} = values;
+    d3.json(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=942TX8GNKGBPPZ1J`)
+      .then(data => {
+        this.setState({data: data['Time Series (5min)']});
+      });
   };
 
   render() {
