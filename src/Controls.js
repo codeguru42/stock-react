817b8680b7cf6082cfd17de644f5fa7e7331edc3
Code Apprentice <codeguru42@gmail.com>
2018-09-29 16:15:02 -0600

    Add some controls

--- src/Controls.js
+++ src/Controls.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 export class Controls extends Component {
+  onSubmit = event => {
+    alert('Submitted!');
+  };
+
   render() {
-    return <div/>;
+    return (
+      <div>
+        <TextField
+          label={'Stock Symbol:'}
+          name={'symbol'}
+        />
+        <Button
+          variant={'contained'}
+          onClick={this.onSubmit}
+        >
+          Submit
+        </Button>
+      </div>
+    );
   }
 }
